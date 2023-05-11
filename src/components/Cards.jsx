566ef6b3e529a473532da4fd198ee346de766c5d
Ale Miranda <alemiranda2511@gmail.com>
2023-05-11 00:37:46 -0300

    Update

---
+++ src/components/Cards.jsx
@@ -0,0 +1,25 @@
+import Card from "./Card";
+
+export default function Cards(props) {
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map((character) => {
+        return (
+          <Card
+            key={character.id}
+            name={character.name}
+            status={character.status}
+            species={character.species}
+            gender={character.gender}
+            origin={character.origin.name}
+            image={character.image}
+            onClose={() => {
+              window.alert("Character Close");
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+}
